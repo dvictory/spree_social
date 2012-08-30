19500d3875fcaa8cdeaebdce428a58c14def0444
Derek Victory <derekvictory@yahoo.com>
2012-08-30 16:48:51 -0500

    Fix double render

--- app/controllers/spree/user_registrations_controller_decorator.rb
+++ app/controllers/spree/user_registrations_controller_decorator.rb
@@ -2,11 +2,15 @@ Spree::UserRegistrationsController.class_eval do
   ssl_allowed :create
   def create
     build_resource(params[:user])
-    check_for_promo
-    if @user.save
+    promo_ok = check_for_promo
+    if !promo_ok
+      @user.errors.add(:signup_code,"Invalid sign up code")
+      clean_up_passwords(@user)
+      render :new
+    elsif @user.save
       set_flash_message(:notice, :signed_up)
       sign_in(:user, @user)
-      fire_event('spree.user.signup', :user => @user, :order => current_order(true))
+      fire_event('spree.user.signup', :user => @user, :order => current_order(true)) if promo_ok
       sign_in_and_redirect(:user, @user)
     else
       clean_up_passwords(@user)
@@ -27,13 +31,6 @@ Spree::UserRegistrationsController.class_eval do
   end
 
   def check_for_promo
-    if @user.respond_to?(:signup_code) and !@user.signup_code.blank?
-      if @user.signup_code.upcase != "DSFREE5"
-        clean_up_passwords(@user)
-        render :new
-        session[:omniauth] = nil unless @user.new_record?
-        return
-      end
-    end
+    @user.respond_to?(:signup_code) and !@user.signup_code.blank? and @user.signup_code.upcase == "DSFREE5"
   end
 end
